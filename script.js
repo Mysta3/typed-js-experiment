f7d772820b2ff89c8ec89f0f7fd4092dc06f4a54
Mysta3 <myshawnestallings@gmail.com>
2020-09-02 13:42:16 -0700

    added styling, neon text, created a box for outputing text

--- script.js
+++ script.js
@@ -2,5 +2,6 @@
 
 var typed = new Typed('.typed_text', {
   // Waits 1000ms after typing "First"
-  strings: ['Test 1234', 'Test Checkmate', 'Checkers Not Chess'],
+  strings: [' Test 5 ^1000', 'First ^1000 sentence.', 'Second sentence.'],
+  smartBackspace: true, // Default value
 });
