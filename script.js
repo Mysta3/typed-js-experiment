3237e05c5281bff450929a5591dc566094be91f2
Mysta3 <myshawnestallings@gmail.com>
2020-09-02 23:05:16 -0700

    added background image, added more story to page and added attributes

--- script.js
+++ script.js
@@ -4,10 +4,24 @@ var typed = new Typed('.typed_text', {
     ' Hi ^1000 and Welcome...',
     '...^1000 to the Matrix...',
     '🔥',
-    'One day a young king decided to do better for himself',
+    'Let me tell you a story..',
+    'One day a young kid was obsessed with computers...',
+    "his father didn't see his gift...",
+    'BUT',
+    "It was nutured through the boy's own curiosity!",
+    'years later he graduates',
+    'years later he moves to Cali',
+    'years later he joins a bootcamp',
+    'and goes from Desktop Tech to Web Developer....',
+    'THE....',
+    'THE END',
   ],
   //backspaces the part of the sentence that isn't the same as the previous.
   smartBackspace: true, // Default value
   //measured in milliseconds
   typeSpeed: 100,
+  //delay animation
+  startDelay: 2,
+  //backspacing speed
+  backSpeed: 2,
 });
