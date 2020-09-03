f21ff9bb95270d2ca1f4e4654b0fd9bc556d14e9
Mysta3 <myshawnestallings@gmail.com>
2020-09-02 18:42:31 -0700

    added more sentences to the strings array, added styling

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-
-
 var typed = new Typed('.typed_text', {
   // Waits 1000ms after typing "First"
-  strings: [' Test 5 ^1000', 'First ^1000 sentence.', 'Second sentence.'],
+  strings: [
+    ' Hi ^1000 and Welcome...',
+    '...^1000 to the Matrix...',
+    '🔥',
+    'One day a young king decided to do better for himself',
+  ],
+  //backspaces the part of the sentence that isn't the same as the previous.
   smartBackspace: true, // Default value
+  //measured in milliseconds
+  typeSpeed: 100,
 });
